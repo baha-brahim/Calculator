50258224235859523dbad9a6593c2f9c13bf3b22
Baha Brahim <bahbouhabrahim2@gmail.com>
2024-07-04 05:13:12 -0700

    this time still the calculate function seriously did all the things necessary for this function to work properly

--- script.js
+++ script.js
@@ -27,29 +27,51 @@ calculate2.addEventListener("mouseout" ,() => {
 //Writing inside the input :
 let screen = document.querySelector("#screen");
 let buttons = document.querySelectorAll(".btn");
-
 buttons.forEach(button => {  
+    let nb = 0 // this variable helps in determining if we would have to calculate or not.
     button.addEventListener("click" ,function() {
+
         let value = this.textContent;
+        if (["-" ,"0"].includes(screen.textContent) && ["x" ,"÷" ,"+"].includes(value)) value = "";
         if (value === "Clear") {
             screen.textContent = "";
             value = "";
         }
+        if (["x", "÷", "+", "-"].includes(value)) {
+            let array = []
+            let i = 0;
+            ch = ["x", "÷", "+", "-"];
+            do {
+                array = screen.textContent.split(ch[i]);
+                i+=1;
+            } while (array.length === 1 && i < 4);
+            
+            if (array.length === 2) {
+                value = calculate();
+            }
+        } 
         if (value === "=") {
             value = calculate();
         }
-        if (value === "del") {
+        if (value === "del" && screen.textContent.length >=1 && screen.textContent[0] !== "0") {
             screen.textContent = screen.textContent.slice(0,-1);
             value = "";
         }
+        if (value === "del") value = ""
         if (["+","-","%","x","÷"].includes(screen.textContent.slice(-1)) && ["+","-","%","x","÷"].includes(value)) {
             screen.textContent = screen.textContent.slice(0,-1);
         }
-        if (screen.textContent.length < 11) {
+        if (screen.textContent.length < 11 && screen.textContent[0] !== "0") {
             screen.textContent += value;
+        } else {
+            screen.textContent = value;
         }
+        if (screen.textContent === "") screen.textContent = "0";
     }) 
 })
 
 
 //Calculate:
+function calculate() {
+    return "="
+}
